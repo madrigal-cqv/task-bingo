2c308c831884b5fd7e63ca2e376eab1dc3386635
Chi Vu <vu000153@umn.edu>
2025-08-05 17:05:54 -0700

    Figuring out how to read inputs. Add some other design thoughts

--- design_thoughts.md
+++ design_thoughts.md
@@ -1,9 +1,10 @@
 # Task Bingo
 - store data in a json file (not great for obvious reason, but works fine enough for a small sample webapp)
-- data included: username, password, number of tasks, layout, list of tasks
+- data included: username, password, number of tasks, layout, list of tasks, done tasks
 - new/returning user, unique username prompted for new, username not found for returning. wrong password
 - one page, changing pop-ups
 - session if available
 - 9/16/25 avaible (if 9/25, middle is free space)
 - reshuffle
 - bingo notification
+- time limit?
--- web/main.dart
+++ web/main.dart
@@ -1,9 +1,24 @@
 import 'package:web/web.dart' as web;
 
+void generateBoard(int numTasks, web.HTMLDivElement bingo) {
+  switch (numTasks) {
+    case 9:
+      break;
+    case 16:
+      break;
+    case 25:
+      break;
+  }
+}
+
 void main() {
   final now = DateTime.now();
   final element = web.document.querySelector('#output') as web.HTMLDivElement;
-  element.text =
-      'The time is ${now.hour}:${now.minute} '
-      'and your Dart web app is running!';
+  final bingo = web.document.querySelector("#bingo-card") as web.HTMLDivElement;
+  var numTasks =
+      web.document.querySelector("#numTasks") as web.HTMLSelectElement;
+  numTasks.onChange.listen(
+    (data) => element.text =
+        'This exists at ${now.hour}:${now.minute}! ${numTasks.value}',
+  );
 }
